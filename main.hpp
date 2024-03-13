e0d7f9e528bef2c41a2ca5b62bcf726f5abcc294
Em21638work <156359825+Em21638work@users.noreply.github.com>
2024-03-13 11:49:23 -0700

    First commit

--- main.hpp
+++ main.hpp
@@ -13,13 +13,51 @@ int getClosestMed(int, int, int);
 // complete two functions
 int getRandom(void)
 {
-        /***************************************************
-         * Code your program here
-         ***************************************************/
+        // my code
+        int limit = 20;
+        int num;
+
+        num = rand() % limit;
+
+        return num;
+        //end of my code
 }
-int getClosestMed(int rdnum1, int rdnum2, int rdnum3)
-{
-        /***************************************************
-         * Code your program here
-         ***************************************************/
+int getClosestMed(int rdnum1, int rdnum2, int rdnum3) {
+        int least, mid;
+        if (rdnum1 < rdnum2) {
+                if (rdnum1 < rdnum3) {
+                        least = rdnum1
+                        if (rdnum2 < rdnum3) {
+                                mid = rdnum2   
+                        }
+                        else {
+                                mid = rdnum3
+                        }
+                }
+        }
+        if (rdnum2 < rdnum1) {
+                if (rdnum2 < rdnum3) {
+                        least = rdnum2
+                        if (rdnum1 < rdnum3) {
+                                mid = rdnum1   
+                        }
+                        else {
+                                mid = rdnum3
+                        }
+                }
+        }
+         if (rdnum3 < rdnum1) {
+                if (rdnum3 < rdnum2) {
+                        least = rdnum3
+                        if (rdnum1 < rdnum2) {
+                                mid = rdnum1   
+                        }
+                        else {
+                                mid = rdnum2
+                        }
+                }
+        }
+
+        return mid;
+
 }
